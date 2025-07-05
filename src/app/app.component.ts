97c08d97657dcff90b45f39fa5076970b013f80d
ashna <ashnacp0204@gmail.com>
2025-07-05 10:19:51 +0530

    fix: include missing component and service files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject ,computed} from '@angular/core';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskService } from './services/task.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TaskFormComponent, TaskListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Task_Manager';
+  taskService = inject(TaskService);
+ private tasks = this.taskService.tasks;
+
+  todoItems = computed(() => {
+    const tasks = this.tasks();
+    return tasks.filter((x) => x.status === 'Todo');
+  });
+  inProgressItems = computed(() => {
+    const tasks = this.tasks();
+    return tasks.filter((x) => x.status === 'Inprogress');
+  });
+  completedItems = computed(() => {
+    const tasks = this.tasks();
+    return tasks.filter((x) => x.status === 'completed');
+  });
 }
